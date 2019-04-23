44a37d90359f2ff53a9b42d2f86f7de253a546b4
Guita1538 <domiccover@users.noreply.github.com>
2019-04-23 14:37:42 +0700

    test

--- bodyparser/app.js
+++ bodyparser/app.js
@@ -6,7 +6,7 @@ var pug = require('pug')
 var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/index');  
 var users = require('./routes/users');
 var app = express();
 var forms = require('./routes/forms');
