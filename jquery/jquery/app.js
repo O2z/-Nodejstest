81598ba0ecb768d9ba37540460f2b59c8716a6c7
Guita1538 <domiccover@users.noreply.github.com>
2019-04-23 15:02:45 +0700

    AS

---
+++ jquery/jquery/app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var pug = require('pug')
+var path = require('path');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.get('/login', function(req, res){
+  res.sendFile('public/login.html',{ root:__dirnamefcc });
+}); 
+
+app.post('/login', function (req, res){
+  var username = req.body.txtUsername;
+  var password = req.body.txtPassword; 
+  console.log("Username: " + username + "\nPassword: " + password);
+  res.end("yes");
+});
+app.use(function(req, res, next){
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if(app.get('env') === 'development'){
+  app.use(function (err, req, res, next){
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+     });
+  });
+}
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+module.exports = app;
